9020c51838d580b4d47fa0701a27b2b04eba6d2c
patrickhebner <patrick.hebner@exxeta.com>
2024-09-20 12:42:20 +0200

    chore: add snapshot

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -2,5 +2,5 @@
 docker exec -t directus npx directus schema snapshot /tmp/snapshot.yml
 docker cp directus:/tmp/snapshot.yml snapshot.yml
 today=$(date +"%Y_%m_%d")
-cp snapshot.yml snapshots/snapshot_${today}.yml
+mv snapshot.yml snapshots/snapshot_${today}.yml
 
